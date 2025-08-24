bccabe63976cdf9ae416556da46de3a4c366ab4f
Siddharth Garg <siddharthgarg16082000@gmail.com>
2025-08-24 15:23:49 +0530

    configured express app instance for server.ts

--- server.ts
+++ server.ts
@@ -1,1 +1,13 @@
-console.log("Welcome");
+import dotenv from "dotenv";
+import APP from "./src/app";
+
+dotenv.config();
+
+const startServer = (): void => {
+  const PORT = process.env.PORT || 3000;
+  APP.listen(PORT, () => {
+    console.log(`Server running on PORT: ${PORT}`);
+  });
+};
+
+startServer();
