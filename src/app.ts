da69dc08df9c72075b09a98808be6827474174bc
Siddharth Garg <siddharthgarg16082000@gmail.com>
2025-08-26 22:35:41 +0530

    added global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,15 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import errorHandler from "./middlewares/error-handler.middleware";
 
 const APP = express();
+
+APP.get("/", (req: Request, res: Response, next: NextFunction) => {
+  //   const error = createHttpError(400, "ungabunga");
+  //   throw error;
+  res.json({ message: "App is running" });
+});
+
+// global error handler middleware
+APP.use(errorHandler);
 
 export default APP;
---
+++ src/middlewares/error-handler.middleware.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from "express";
+import { HttpError } from "http-errors";
+import { config } from "../config/config";
+
+const errorHandler = (
+  err: HttpError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const statusCode = err.statusCode || 500;
+
+  return res.status(statusCode).json({
+    message: err.message,
+    errorStack: config.env === "development" ? err.stack : "", // not in production
+    status: statusCode,
+  });
+};
+
+export default errorHandler;
